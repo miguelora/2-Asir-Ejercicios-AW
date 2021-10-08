dabe691970eabd0e1cac10d6400b2db1b1f9e4c5
Miguel <miguel_romerillo@hotmail.com>
2021-10-08 12:31:18 +0200

    08-octubre V2

--- horario.php
+++ horario.php
@@ -72,28 +72,95 @@
     
     
     
-    
-    
-                    switch ($hora) {
-                        case 'Monday':
-                            echo "Aplicaciones web"."Horario: 8:15-11:15";
-                            echo "Recreo"."<br>"."Horario: 11:15-11:45";
-                            echo "Servicios red"."<br>"."Horario: 11:45-14:45";
-                            break;
-                        
-                        case 'Tuesday':
-                            echo "Administracion SSOO"."<br>"."Horario: 8:15-11:15";
-                            echo "Recreo"."<br>"."Horario: 11:15-11:45";
-                            echo "Seguridad"."<br>"."Horario: 11:45-12:45";
-                            echo "Empresa"."<br>"."Horario: 12:45-14:45";
-                            break;
-            
-                        case 'Wednesday':
-                            echo "Administracion SSOO"."<br>"."Horario: 8:15-11:15";
-                            echo "Recreo"."<br>"."Horario: 11:15-11:45";
-                            echo "Servicios red"."<br>"."Horario: 11:45-14:45";
-                            break;
-    
+         switch ($semana) {
+             case 'Monday':
+                 switch ($hora) {
+                     case '08':
+                         echo "Tenemos hora de Aplicaciones Web"
+                         break;
+                     
+                     case '09':
+                         echo "Tenemos hora de  Aplicaciones Web"
+                         break;
+
+                    case '10':
+                        echo "Tenemos hora de Aplaciones Web"
+                        break;
+                    
+                    case '11':
+                        echo "Tenemos hora de Recreo"
+                        break;
+
+                    case "12":
+                        echo "Tenemos hora de Servicios Red"
+                        break;
+
+                    case "13":
+                        echo "Tenemos hora de Servicios Red"
+                        break;
+
+                    case "14":
+                         echo "Tenemos hora de Servicios Red"
+                        break;
+
+                    case "15":
+                        echo "FIN DE CLASES"
+                        break;
+                 }
+                 break;
+             
+            case 'Tuesday':
+                switch ($hora) {
+                    case '08':
+                        echo "Tenemos hora de Administracion de SS.OO"
+                        break;
+                    
+                    case '09':
+                        echo "Tenemos hora de  Administracion de SS.OO"
+                        break;
+
+                   case '10':
+                       echo "Tenemos hora de Administracion de SS.OO"
+                       break;
+                   
+                   case '11':
+                       echo "Tenemos hora de Recreo"
+                       break;
+
+                   case "12":
+                       echo "Tenemos hora de Seguridad"
+                       break;
+
+                   case "13":
+                       echo "Tenemos hora de Empresa"
+                       break;
+
+                   case "14":
+                        echo "Tenemos hora de Empresa"
+                       break;
+
+                   case "15":
+                       echo "FIN DE CLASES"
+                       break;
+                }
+                 
+                 break;
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+                }
     
     
     
