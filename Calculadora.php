ebf4f46fa82b7073999ab7a29511b18eb92dd744
Miguel <miguel_romerillo@hotmail.com>
2021-10-11 10:56:17 +0200

    11-octubre v1

---
+++ Calculadora.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+
+
+    <fieldset>
+        <legend>Calculadora Asir</legend>
+        
+
+    
+
+
+    <p>Número 1: <input type=int name="Número 1" value="0" required></p>
+
+    <p>Número 2:
+        <input type="int" name="Número 2" value="0" required>
+    </p>
+    <button name="Sum" type="submit">+</button>
+    <button name="Res" type="submit" >-</button>
+    <button name="Mul" type="submit">*</button>
+    <button name="Div" type="submit">/</button>
+    <button name="Mol" type="submit">%</button>
+    <button name="C" type="reset">C</button>
+    <br><br>
+    <button name="OP">Operar</button>
+
+    </fieldset>
+    
+    <h2 style="color: rgb(14, 224, 25);">RESULTADO: </h2>
+
+
+
+
+</body>
+</html>
