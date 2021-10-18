b1c7e61feae000facbb557f4740bd31f426e2833
Miguel <miguel_romerillo@hotmail.com>
2021-10-18 08:57:18 +0200

    18-octubre v2

--- Calculadora.php
+++ Calculadora.php
@@ -19,14 +19,21 @@
     <p>Número 1: <input type=int name="Número 1" value="0" required></p>
 
     <p>Número 2:
-        <input type="int" name="Número 2" value="0" required>
-    </p>
+        <input type="int" name="Número 2" value="0" required></p>
+    
+    <p>Número exp: <input type=int name="Número exp" value="0" required></p>
+    
     <button name="Sum" type="submit">+</button>
     <button name="Res" type="submit" >-</button>
     <button name="Mul" type="submit">*</button>
     <button name="Div" type="submit">/</button>
     <button name="Mol" type="submit">%</button>
+    <button name="cua" type="submit">√</button>
+    <button name="cub" type="submit">∛</button>
+    <button name="exp" type="submit">exp</button>
     <button name="C" type="reset">C</button>
+    
+    
     <br><br>
     <button name="OP">Operar</button>
 
@@ -75,6 +82,49 @@
             
 
         }
+        
+        function cua($num3){
+
+            $res = pow($num3, 2);
+
+            
+
+        }
+
+        function cub($num1,$num2){
+
+            $res = pow($num3, 3);
+
+            
+
+        }
+
+        function exponente($num1,$num3){
+
+            $res = pow($num1, $num3);
+
+            
+
+        }
+
+
+        #SERIE FINONACCI#
+
+        function fibonacci($n)
+        {
+            $fibonacci  = [0,1];
+ 
+            for($i=2;$i<=$n;$i++)
+        {
+                $fibonacci[] = $fibonacci[$i-1]+$fibonacci[$i-2];
+         }
+            echo $fibonacci[$n];
+        }
+
+
+    fibonacci(10);
+
+
 
 
 
