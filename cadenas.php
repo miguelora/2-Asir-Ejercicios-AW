a282d9dd4816a0e8644868ec4996545cdf0a7b87
Miguel <miguel_romerillo@hotmail.com>
2021-10-29 12:34:04 +0200

    29-octubre v1

---
+++ cadenas.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cadenas</title>
+</head>
+<body>
+    <?php
+
+        $recoge1 = $_POST["texto1"];
+        $recoge2 = $_POST["texto2"];
+        $recoge3 = $_POST["texto3"];
+        $operador = $_POST["despliega"];
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ?>
+
+
+
+    
+</body>
+</html>
