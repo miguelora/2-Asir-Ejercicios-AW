ecfcea79f814a07e8bd7bea2ab15bda19c46d92b
Miguel <miguel_romerillo@hotmail.com>
2022-01-14 12:37:33 +0100

    14-01-2022

---
+++ repasito/constantes.php
@@ -0,0 +1,40 @@
+<?php
+
+##Definición de constantes##
+
+define("Gravedad",9.8);
+define("PI",3.4);
+
+function suma($n1,$n2){
+    $res=$n1+$n2;
+    echo "El resultado es: ".$res;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ repasito/prueba_includes.php
@@ -0,0 +1,35 @@
+<?php
+include_once 'constantes.php';
+
+
+echo "La vida sigue";
+echo "<br>";
+echo Gravedad;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
