4e45543f51e7f320709e1503ed44dc7377b54123
Miguel <miguel_romerillo@hotmail.com>
2022-01-25 13:54:58 +0100

    25-01-2022 v2

--- Práctica enero/arraydb.php
+++ Práctica enero/arraydb.php
@@ -1,7 +1,18 @@
 <?php
 
 
-
+$destino=array(
+    'Vueling-AE12'=>'Milan',
+    'Volotea-EA21'=>'Roma',
+    'Iberia-WE44'=>'Estambul',
+    'Ryanair-EW44'=>'Madrid',
+    'Vueling-ZK87'=>'Lima',
+    'Volotea-UA21'=>'Bilbao',
+    'Iberia-WH44'=>'Roma',
+    'Ryanair-EL00'=>'Madrid',
+    'Vueling-AE12'=>'Roma',
+    'Volotea-EA21'=>'Roma'
+)
 
 
 
