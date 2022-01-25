f520b31377ad415f1929a5fc45e39c66b99b8977
Miguel <miguel_romerillo@hotmail.com>
2022-01-25 14:18:31 +0100

    25-01-2022 arrays acabados

--- Práctica enero/arraydb.php
+++ Práctica enero/arraydb.php
@@ -14,9 +14,55 @@ $destino=array(
     'Volotea-EA21'=>'Roma'
 )
 
+$fabricante=array(
+    'Vueling-AE12'=>'Boeing',
+    'Volotea-EA21'=>'Airbus',
+    'Iberia-WE44'=>'Airbus',
+    'Ryanair-EW44'=>'Boeing',
+    'Vueling-ZK87'=>'Airbus',
+    'Volotea-UA21'=>'Boeing',
+    'Iberia-WH44'=>'Boeing',
+    'Ryanair-EL00'=>'Airbus',
+)
 
+$duracion=array(
+    'Vueling-AE12'=>'120',
+    'Volotea-EA21'=>'100',
+    'Iberia-WE44'=>'340',
+    'Ryanair-EW44'=>'45',
+    'Vueling-ZK87'=>'600',
+    'Volotea-UA21'=>'65',
+    'Iberia-WH44'=>'90',
+    'Ryanair-EL00'=>'50',
+    'Vueling-AE12'=>'115',
+    'Volotea-EA21'=>'100',
+    'Iberia-WE44'=>'85',
+    'Ryanair-EW44'=>'360'
+)
 
-
+$pasajeros=array(
+    'Vueling-AE12'=>'90',
+    'Volotea-EA21'=>'75',
+    'Iberia-WE44'=>'100',
+    'Ryanair-EW44'=>'60',
+    'Vueling-ZK87'=>'77',
+    'Volotea-UA21'=>'93',
+    'Iberia-WH44'=>'86',
+    'Ryanair-EL00'=>'Madr75id',
+    'Vueling-AE12'=>'87',
+    'Volotea-EA21'=>'97',
+    'Iberia-WE44'=>'81'
+    'Ryanair-EW44'=>'75',
+    'Vueling-ZK87'=>'87',
+    'Volotea-UA21'=>'90',
+    'Iberia-WH44'=>'44',
+    'Ryanair-EL00'=>'67',
+    'Vueling-AE12'=>'86',
+    'Volotea-EA21'=>'66',
+    'Iberia-WE44'=>'40',
+    'Ryanair-EW44'=>'77',
+    'Vueling-ZK87'=>'82'
+)
 
 
 
