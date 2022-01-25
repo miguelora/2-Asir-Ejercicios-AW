5edd87c4ee41d4b2d33082f61987ad4bde1bead4
Miguel <miguel_romerillo@hotmail.com>
2022-01-25 13:08:23 +0100

    25-01-2022

---
+++ Práctica enero/intermedio_d_principal.php
@@ -0,0 +1,36 @@
+<?php
+
+
+$selector=$_POST["selector"]
+
+
+
+
+switch ($selector) {
+    case 'muestra_vuelo':
+        <a 
+        break;
+    
+    default:
+        # code...
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
