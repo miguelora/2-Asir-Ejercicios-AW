c30bd582df47b17cf2f69d7b7ef0de22fff7f969
miguelora <miguel_romerillo@hotmail.com>
2022-02-14 13:45:48 +0100

    prueba

--- Práctica Febrero/ticketBBDD.php
+++ Práctica Febrero/ticketBBDD.php
@@ -2,7 +2,70 @@
 
 
 
+$destino=array(
+    array('Vuelo'=>'Vueling-AE12','Desti'=>'Milán'),
+    array('Vuelo'=>'Volotea-EA21','Desti'=>'Roma'),
+    array('Vuelo'=>'Iberia-WE44','Desti'=>'Estambul'),
+    array('Vuelo'=>'Ryanair-EW44','Desti'=>'Madrid'),
+    array('Vuelo'=>'Vueling-ZK87','Desti'=>'Lima'),
+    array('Vuelo'=>'Volotea-UA21','Desti'=>'Bilbao'),
+    array('Vuelo'=>'Iberia-WH44','Desti'=>'Roma'),
+    array('Vuelo'=>'Ryanair-EL00','Desti'=>'Madrid'),
+    array('Vuelo'=>'Vueling-AE12','Desti'=>'Roma'),
+    array('Vuelo'=>'Volotea-EA21','Desti'=>'Roma'),
 
+);
+
+$fabricante=array(
+    array('Vuelo'=>'Vueling-AE12','fabric'=>'Boeing'),
+    array('Vuelo'=>'Volotea-EA21','fabric'=>'Airbus'),
+    array('Vuelo'=>'Iberia-WE44','fabric'=>'Airbus'),
+    array('Vuelo'=>'Ryanair-EW44','fabric'=>'Boeing'),
+    array('Vuelo'=>'Vueling-ZK87','fabric'=>'Airbus'),
+    array('Vuelo'=>'Volotea-UA21','fabric'=>'Boeing'),
+    array('Vuelo'=>'Iberia-WH44','fabric'=>'Boeing'),
+    array('Vuelo'=>'Ryanair-EL00','fabric'=>'Airbus'),
+);
+
+$duracion=array(
+    array('Vuelo'=>'Vueling-AE12','Minutos'=>'120'),
+    array('Vuelo'=>'Volotea-EA21','Minutos'=>'100'),
+    array('Vuelo'=>'Iberia-WE44','Minutos'=>'340'),
+    array('Vuelo'=>'Ryanair-EW44','Minutos'=>'45'),
+    array('Vuelo'=>'Vueling-ZK87','Minutos'=>'600'),
+    array('Vuelo'=>'Volotea-UA21','Minutos'=>'65'),
+    array('Vuelo'=>'Iberia-WH44','Minutos'=>'90'),
+    array('Vuelo'=>'Ryanair-EL00 ','Minutos'=>'50'),
+    array('Vuelo'=>'Vueling-AE12','Minutos'=>'115'),
+    array('Vuelo'=>'Volotea-EA21','Minutos'=>'100'),
+    array('Vuelo'=>'Iberia-WE44','Minutos'=>'85'),
+    array('Vuelo'=>'Ryanair-EW44','Minutos'=>'360'),
+    
+);
+
+$pasajeros=array(
+    array('Vuelo'=>'Vueling-AE12','Personas'=>'90'),
+    array('Vuelo'=>'Volotea-EA21','Personas'=>'75'),
+    array('Vuelo'=>'Iberia-WE44','Personas'=>'100'),
+    array('Vuelo'=>'Ryanair-EW44','Personas'=>'60'),
+    array('Vuelo'=>'Volotea-UA21','Personas'=>'93'),
+    array('Vuelo'=>'Iberia-WH44','Personas'=>'86'),
+    array('Vuelo'=>'Ryanair-EL00','Personas'=>'75'),
+    array('Vuelo'=>'Vueling-AE12','Personas'=>'87'),
+    array('Vuelo'=>'Volotea-EA21','Personas'=>'97'),
+    array('Vuelo'=>'Iberia-WE44','Personas'=>'81'),
+    array('Vuelo'=>'Ryanair-EW44','Personas'=>'75'),
+    array('Vuelo'=>'Vueling-ZK87','Personas'=>'87'),
+    array('Vuelo'=>'Volotea-UA21','Personas'=>'90'),
+    array('Vuelo'=>'Iberia-WH44','Personas'=>'84'),
+    array('Vuelo'=>'Ryanair-EL00','Personas'=>'67'),
+    array('Vuelo'=>'Vueling-AE12','Personas'=>'86'),
+    array('Vuelo'=>'Volotea-EA21','Personas'=>'66'),
+    array('Vuelo'=>'Ryanair-EL00','Personas'=>'75'),
+    array('Vuelo'=>'Iberia-WE44','Personas'=>'40'),
+    array('Vuelo'=>'Ryanair-EW44','Personas'=>'77'),
+    array('Vuelo'=>'Vueling-ZK87','Personas'=>'82'),
+);
 
 
 
